e525d17702a10369fa0ab2521b052e7d78915080
rayan mammeri <rayan.mammero@outlook.fr>
2024-09-13 15:33:28 +0200

    Initial Commit

---
+++ SeriesHUBremake/SeriesHUBremakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeriesHUBremakeApp.swift
+//  SeriesHUBremake
+//
+//  Created by Rayan Mammeri on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SeriesHUBremakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
