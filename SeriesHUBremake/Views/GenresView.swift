cb7e64d56ed76087d2cc760eae7e5f2c7b2e0ab5
7nayr <rayan.mammeri8@gmail.com>
2024-09-16 17:07:48 +0200

    amelioration du systeme de connexion, de la page de détails de serie, et de la page de chargement du debut

---
+++ SeriesHUBremake/Views/GenresView.swift
@@ -0,0 +1,8 @@
+//
+//  GenresView.swift
+//  SeriesHUBremake
+//
+//  Created by Rayan Mammeri on 16/09/2024.
+//
+
+import Foundation
---
+++ SeriesHUBremake/Views/LoadingView.swift
@@ -0,0 +1,8 @@
+//
+//  LoadingView.swift
+//  SeriesHUBremake
+//
+//  Created by Rayan Mammeri on 16/09/2024.
+//
+
+import Foundation
